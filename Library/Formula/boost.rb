f3158287defedf2067d701af13f586997a244256
Andrew Hundt <ATHundt@gmail.com>
2015-11-13 02:38:17 -0500

    boost.rb with gcc now compiles boost.log on linux
    eventually it would be ideal to only enable this if the gcc version is >=4.5
    https://github.com/Homebrew/homebrew/issues/45954

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -88,7 +88,7 @@ class Boost < Formula
 
     # Boost.Log cannot be built using Apple GCC at the moment. Disabled
     # on such systems.
-    without_libraries << "log" if ENV.compiler == :gcc || ENV.compiler == :llvm
+    without_libraries << "log" if OS.mac? && (ENV.compiler == :gcc || ENV.compiler == :llvm)
     without_libraries << "mpi" if build.without? "mpi"
 
     bootstrap_args << "--without-libraries=#{without_libraries.join(",")}"
